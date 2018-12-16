e3442c78da77d2a3cd3222372ca40aa33a3a434d
Marc <marc-gw@live.nl>
2018-12-16 15:18:21 +0100

    updated design + cleaned code

--- app/src/main/java/com/potatohead/mr/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/potatohead/mr/mrpotatohead/MainActivity.java
@@ -7,8 +7,6 @@
 
 package com.potatohead.mr.mrpotatohead;
 
-import android.media.Image;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
